82beab9fa04c6b674e6dbe534507e1745c535e24
allObjects <markus@muet.com>
2015-09-14 10:50:29 -0700

    fix gain setting in config register before conversion in getADC
    To set gain in register on convert, use ‘value’ (from ‘table’ object
    GAINS) and not just the ‘pointer’ (this.gain).

--- devices/ADS1X15.js
+++ devices/ADS1X15.js
@@ -120,7 +120,7 @@ ADS1X15.prototype.getADC = function(channel, callback) {
                     CONFIG.DR_1600SPS   | // 1600 samples per second (default)
                     CONFIG.MODE_SINGLE;   // Single-shot mode (default)
   // Set PGA/voltage range
-  config |= this.gain;
+  config |= GAINS[this.gain];
   // Set single-ended input channel
   config |= [CONFIG.MUX_SINGLE_0,CONFIG.MUX_SINGLE_1,CONFIG.MUX_SINGLE_2,CONFIG.MUX_SINGLE_3][channel];
   // Set 'start single-conversion' bit
